5261654feba241e200a92e2f5e8ea7b47ed47224
Zach Ghera <zpghera00@gmail.com>
2018-10-26 11:57:10 -0400

    Engineering Lec Work

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
 # Project2
+
+'''
+1. cost and efficiency functions for pumping up and down
+1.1 pump
+1.2 pipes
+1.3 turbine
+2. Create data sets for cost v efficiency
+
+
+'''
--- main.py
+++ main.py
@@ -1,1 +1,76 @@
-#Project2 Main
+ENERGY_OUT = 120     #KWh
+GRAVITY = 9.81       #m/s^2
+WATER_DENSITY = 1000 #kg/m^3
+import math
+
+
+#function that calculates the cost and efficiency of filling the reservoir
+def filling(vel):
+    
+#function that calculates the cost and efficiency of draining the reservoir
+def draining(vel):
+
+#function that calculates the overall efficiency of the hydropump system
+def efficiency():
+
+#function that calculates the overall cost of the hydropump system
+def cost():
+
+class zone:
+    def __init__(self,zone_depth,zone_elev):
+        self.pipe = 
+        self.depth = depth
+        self.elev = elev        
+
+
+class pipe:
+    def __init__(self,pipe_fr,pipe_d,pipe_l,bend1 = None, bend2 = None):
+        self.pipe_fr = pipe_fr
+        self.pipe_d = pipe_d
+        self.pipe_l = pipe_l
+        self.bend1 = bend1
+
+
+   
+
+class bend:
+    def __init__(self,bend_num, bend_ang,bend_coe):
+        self.bend_num = bend_num
+        self.bend_ang = bend_ang
+        self.bend_coe = bend_coe     
+        
+
+class pump:
+    def __init__(self,pump_ef,pipe,vel):
+        self.pump_ef
+        
+    def pump_flow(self,vel):
+        #return pipe_flow = ((self.pipe.getd ** 2) * vel)/1.273 #from engineering toolbox
+
+
+class turbine:
+    def __init__(self,turbine_ef,pipe,vel):
+        self.turbine_ef
+
+    def turb_flow(self,vel):
+        #return pipe_flow = ((self.pipe.getd ** 2) * vel)/1.273 #from engineering toolbox
+
+
+
+
+if __name__ == '__main__':
+    pump_data = [[]]
+    pumps = []
+    pipe
+    bend1
+    bend2
+    bend3
+    bend4
+    turbine
+    pumphouse
+    road
+    site_prep
+    perimeter_qall
+    pipe_install_cost
+    raised_pipe_cost
+    other_costs
